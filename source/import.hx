a8a03d44c7eed7dd2be6c0e8fd154b4ca642467c
Homura <egzozu.be.bas@gmail.com>
2026-02-14 19:13:04 +0300

    cleanup

--- source/import.hx
+++ source/import.hx
@@ -122,17 +122,6 @@ import mobile.substates.MobileExtraControl;
 import mobile.MobileControlManager;
 //Android
 #if android
-#if legacy_lime
-import extension.androidtools.callback.CallBack as AndroidCallBack;
-import extension.androidtools.content.Context as AndroidContext;
-import extension.androidtools.widget.Toast as AndroidToast;
-import extension.androidtools.os.Environment as AndroidEnvironment;
-import extension.androidtools.Permissions as AndroidPermissions;
-import extension.androidtools.Settings as AndroidSettings;
-import extension.androidtools.Tools as AndroidTools;
-import extension.androidtools.os.Build.VERSION as AndroidVersion;
-import extension.androidtools.os.Build.VERSION_CODES as AndroidVersionCode;
-#else
 import android.callback.CallBack as AndroidCallBack;
 import android.content.Context as AndroidContext;
 import android.widget.Toast as AndroidToast;
@@ -142,7 +131,6 @@ import android.Settings as AndroidSettings;
 import android.Tools as AndroidTools;
 import android.os.Build.VERSION as AndroidVersion;
 import android.os.Build.VERSION_CODES as AndroidVersionCode;
-#end
 #end
 
 import online.backend.Deflection;
